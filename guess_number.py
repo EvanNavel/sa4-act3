1d42a1aa01f07356107da7c57785fefeda18eb77
Robert Evan Patterson <mechapoltergeist333@gmail.com>
2024-02-07 23:50:42 -0600

    Q exit code

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,18 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = input("What number am I thinking of? (Enter 'q' to quit) ")
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
+if guess == 'q':
+    print(f"The number was {number}.")
+elif int(guess) == number:
+    print("Congratulations! You guessed the right number.")
 else:
-   print(f"Sorry! The number was {number}.")
+    while int(guess) != number:
+        print("Sorry! That's not the number I'm thinking of. Try again.")
+        guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+        if guess == 'q':
+            print(f"The number was {number}.")
+            break
+    else:
+        print("Congratulations! You guessed the right number.")
